19f47c2a0d178005a122f841e85592035a7fe6d5
BaltodanoDalhuin <dbaltodano@fwdcostarica.com>
2024-07-24 15:20:22 -0600

    cambios 2

---
+++ js/Regristo.js
@@ -0,0 +1,12 @@
+
+    event.preventDefault();
+    var nombre = document.getElementById("nombre").value;
+    var apellido = document.getElementById("apellido").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+
+    console.log("Nombre: " + nombre);
+    console.log("Apellido: " + apellido);
+    console.log("Correo Electrónico: " + email);
+    console.log("Contraseña: " + password);
